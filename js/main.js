932c96c166a98af32274a48b3f4ccc8517321f87
acolombo09 <acolombo0911@gmail.com>
2023-07-23 21:47:15 +0200

    milestone1_&_2_completed

--- js/main.js
+++ js/main.js
@@ -14,9 +14,9 @@ Creiamo il nostro array di oggetti che rappresentano ciascun post. Ogni post dov
 *Non è necessario creare date casuali*
 *Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)*
 Milestone 2
- - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+- Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
 Milestone 3
- - Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+- Se clicchiamo sul tasto “Mi Piace” cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
 Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
 BONUS
 1. Formattare le date in formato italiano (gg/mm/aaaa)
@@ -27,64 +27,103 @@ Ragioniamo come sempre a step.
 Prima scriviamo nei commenti la logica in italiano e poi traduciamo in codice.
 console.log() è nostro amico.
 Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
- Buon lavoro!
+Buon lavoro!
 */
 
-
 const posts = [
-    {
-        "id": 1,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/300?image=171",
-        "author": {
-            "name": "Phil Mangione",
-            "image": "https://unsplash.it/300/300?image=15"
-        },
-        "likes": 80,
-        "created": "2021-06-25"
+  {
+    "id": 1,
+    "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    "media": "https://unsplash.it/600/300?image=171",
+    "author": {
+      "name": "Phil Mangione",
+      "image": "https://unsplash.it/300/300?image=15"
     },
-    {
-        "id": 2,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=112",
-        "author": {
-            "name": "Sofia Perlari",
-            "image": "https://unsplash.it/300/300?image=10"
-        },
-        "likes": 120,
-        "created": "2021-09-03"
+    "likes": 80,
+    "created": "2021-06-25"
+  },
+  {
+    "id": 2,
+    "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    "media": "https://unsplash.it/600/400?image=112",
+    "author": {
+      "name": "Sofia Perlari",
+      "image": "https://unsplash.it/300/300?image=10"
     },
-    {
-        "id": 3,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=234",
-        "author": {
-            "name": "Chiara Passaro",
-            "image": "https://unsplash.it/300/300?image=20"
-        },
-        "likes": 78,
-        "created": "2021-05-15"
+    "likes": 120,
+    "created": "2021-09-03"
+  },
+  {
+    "id": 3,
+    "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    "media": "https://unsplash.it/600/400?image=234",
+    "author": {
+      "name": "Chiara Passaro",
+      "image": "https://unsplash.it/300/300?image=20"
     },
-    {
-        "id": 4,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=24",
-        "author": {
-            "name": "Luca Formicola",
-            "image": null
-        },
-        "likes": 56,
-        "created": "2021-04-03"
+    "likes": 78,
+    "created": "2021-05-15"
+  },
+  {
+    "id": 4,
+    "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    "media": "https://unsplash.it/600/400?image=24",
+    "author": {
+      "name": "Luca Formicola",
+      "image": null
     },
-    {
-        "id": 5,
-        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        "media": "https://unsplash.it/600/400?image=534",
-        "author": {
-            "name": "Alessandro Sainato",
-            "image": "https://unsplash.it/300/300?image=29"
-        },
-        "likes": 95,
-        "created": "2021-03-05"
-    }
+    "likes": 56,
+    "created": "2021-04-03"
+  },
+  {
+    "id": 5,
+    "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+    "media": "https://unsplash.it/600/400?image=534",
+    "author": {
+      "name": "Alessandro Sainato",
+      "image": "https://unsplash.it/300/300?image=29"
+    },
+    "likes": 95,
+    "created": "2021-03-05"
+  }
 ];
+
+// Devo creare un ciclo per stampare i post usando il layout di esempio nell’html
+const container = document.getElementById("container");
+
+// sotto consiglio di Florian, modifico l'intero contenuto del div post
+// copiando il layout html base e cambiando solo i riferimenti
+
+posts.forEach((post) => {
+container.innerHTML +=
+`<div class="post">
+<div class="post__header">
+  <div class="post-meta">                    
+    <div class="post-meta__icon">
+      <img class="profile-pic" src="${post.author.image}" alt="Phil Mangione">                    
+    </div>
+    <div class="post-meta__data">
+      <div class="post-meta__author">${post.author.name}</div>
+      <div class="post-meta__time">${post.created}</div>
+    </div>                    
+  </div>
+</div>
+<div class="post__text">${post.content}</div>
+<div class="post__image">
+  <img src="${post.media}" alt="">
+</div>
+<div class="post__footer">
+  <div class="likes js-likes">
+    <div class="likes__cta">
+      <a class="like-button  js-like-button" href="#" data-postid="1">
+        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+        <span class="like-button__label">Mi Piace</span>
+      </a>
+    </div>
+    <div class="likes__counter">
+      Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+    </div>
+  </div> 
+</div>            
+</div>`
+})
